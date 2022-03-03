8491b8eabbd23a056364c955259843dd97c79ce0
Kazuki Yamada <koukun0120@gmail.com>
2022-03-03 23:07:10 +0900

    update: minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,31 @@
+const htmlmin = require('html-minifier-terser');
+
+const minifyHtmlTransform = (content, outputPath) => {
+   if(outputPath && outputPath.endsWith('.html')) {
+      return htmlmin.minify(content,  {
+         // オプション参考: https://github.com/terser/html-minifier-terser#options-quick-reference
+         useShortDoctype: true,
+         removeComments: true,
+         collapseWhitespace: true,
+         minifyCSS: true,
+         minifyJS: true,
+         maxLineLength: 1000
+      });
+   }
+
+   return content;
+}
+
 module.exports = function(eleventyConfig) {
+
+   // minify
+   eleventyConfig.addTransform('minify html', minifyHtmlTransform);
+
    return {
       dir: {
          input: "src",
          output: "public",
       },
-      templateFormats: ["html", "njk", "md", "11ty.js"],
       htmlTemplateEngine: "njk",
-      markdownTemplateEngine: "njk",
    }
 };
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# yamadashy.github.io
+# [yamadashy.github.io](https://yamadashy.github.io/)
 
 ## 開発
 サイトの生成は [Eleventy](https://www.11ty.dev/) を使用

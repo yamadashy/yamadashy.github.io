f41a158474b67db84b7de657cd43d0e0c90d65a6
Kazuki Yamada <koukun0120@gmail.com>
2022-03-03 23:19:27 +0900

    update: サイトマップなど

---
+++ src/_data/constants.js
@@ -0,0 +1,15 @@
+const siteUrlStem = 'https://yamadashy.github.io';
+const siteUrl = `${siteUrlStem}/`;
+
+module.exports = {
+  // サイト設定
+  siteUrl: `${siteUrl}`,
+  siteUrlStem: siteUrlStem,
+  siteTitle: 'yamadashy',
+  siteDescription: 'yamadashy について',
+  author: 'yamadashy',
+
+  // Google Analytics系
+  googleSiteVerification: 'GPLvXv8kYtLMW912ZS54DKFEZL6ruOrjOFLdHVTo37o',
+  globalSiteTagKey: 'G-SHSQ2GZCPE',
+};

979853a332cd7c56a5ab251104bd4bdd58303a8c
Kazuki Yamada <koukun0120@gmail.com>
2022-03-11 23:59:05 +0900

    update: 画像キャッシュ

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,30 @@
 const htmlmin = require('html-minifier-terser');
+const Image = require('@11ty/eleventy-img');
+
+const imageShortcode = async (src, alt, pathPrefix = '', widths = [400]) => {
+   let metadata = await Image(src, {
+      widths: widths,
+      formats: ["webp", "jpeg"],
+      outputDir: 'public/images',
+      urlPath: `${pathPrefix}images/`,
+      cacheOptions: {
+         duration: '1d',
+      },
+      sharpWebpOptions: {
+         quality: 50,
+      },
+      sharpJpegOptions: {
+         quality: 70,
+      }
+   });
+
+   return Image.generateHTML(metadata, {
+      alt,
+      sizes: '100vw',
+      loading: 'lazy',
+      decoding: 'async',
+   });
+}
 
 const minifyHtmlTransform = (content, outputPath) => {
    if(outputPath && outputPath.endsWith('.html')) {
@@ -19,6 +45,9 @@ const minifyHtmlTransform = (content, outputPath) => {
 module.exports = function(eleventyConfig) {
    // static assets
    eleventyConfig.addPassthroughCopy('src/images');
+
+   // images
+   eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
    // minify
    eleventyConfig.addTransform('minify html', minifyHtmlTransform);
